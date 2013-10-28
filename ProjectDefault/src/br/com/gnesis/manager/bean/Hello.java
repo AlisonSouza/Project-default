37543a49f85458e4837e6dc148d6f3eb1dfe1261
AlisonSouza <alisonrodrigues054@gmail.com>
2013-10-28 21:31:38 -0200

    Criação do Projeto

---
+++ ProjectDefault/src/br/com/gnesis/manager/bean/Hello.java
@@ -0,0 +1,20 @@
+package br.com.gnesis.manager.bean;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@RequestScoped
+@ManagedBean
+public class Hello {
+
+	@PostConstruct
+	public void init() {
+		System.out.println(" Bean executado! ");
+	}
+
+	public String getMessage() {
+		return "Hello World JSF!";
+	}
+
+}
